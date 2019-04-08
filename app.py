8b272448715fde72c92ff5272a9d98c7323ae2bb
rachel-ng <rng@stuy.edu>
2019-04-08 19:30:27 -0400

    file routing

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
+import os
 
 app = Flask(__name__)
 app.static_url_path='/static'
@@ -6,6 +7,10 @@ app.static_url_path='/static'
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/data/data_source0.csv')
+def download_file():
+    return send_file(os.path.join(os.getcwd(), 'data/data_source0.csv'), attachment_filename='data_source0.csv')
 
 if __name__ == "__main__":
     app.run()
--- static/svg.js
+++ static/svg.js
@@ -7,7 +7,7 @@ butt.addEventListener("click", function(e)
 		      {
 			  d3.queue()
 			      .defer(d3.json, "https://d3js.org/us-10m.v2.json")
-			      .defer(d3.csv, "static/data/data_source0.csv")
+			      .defer(d3.csv, "data/data_source0.csv")
 			      .await(ready);
 
 		      });
@@ -53,7 +53,7 @@ var div = d3.select("body").append("div")
 
 d3.queue()
     .defer(d3.json, "https://d3js.org/us-10m.v2.json")
-    .defer(d3.csv, "static/data/data_source0.csv")
+    .defer(d3.csv, "data/data_source0.csv")
     .await(ready);
 
 var path = d3.geoPath()
@@ -247,7 +247,7 @@ document.getElementById("fbw").addEventListener('click', function(e) {
 		}
 		d3.queue()
 				.defer(d3.json, "https://d3js.org/us-10m.v2.json")
-				.defer(d3.csv, "static/data/data_source0.csv")
+				.defer(d3.csv, "data/data_source0.csv")
 				.await(ready);
 	}, 200);
 });
@@ -272,7 +272,7 @@ document.getElementById("bw").addEventListener('click', function(e) {
 		}
 		d3.queue()
 				.defer(d3.json, "https://d3js.org/us-10m.v2.json")
-				.defer(d3.csv, "static/data/data_source0.csv")
+				.defer(d3.csv, "data/data_source0.csv")
 				.await(ready);
 	}, 500);
 });
@@ -297,7 +297,7 @@ document.getElementById("fw").addEventListener('click', function(e) {
 		}
 		d3.queue()
 				.defer(d3.json, "https://d3js.org/us-10m.v2.json")
-				.defer(d3.csv, "static/data/data_source0.csv")
+				.defer(d3.csv, "data/data_source0.csv")
 				.await(ready);
 	}, 500);
 });
@@ -322,7 +322,7 @@ document.getElementById("ffw").addEventListener('click', function(e) {
 		}
 		d3.queue()
 				.defer(d3.json, "https://d3js.org/us-10m.v2.json")
-				.defer(d3.csv, "static/data/data_source0.csv")
+				.defer(d3.csv, "data/data_source0.csv")
 				.await(ready);
 	}, 200);
 });
@@ -351,7 +351,7 @@ document.getElementById("play").addEventListener('click', function(e) {
 			}
 			d3.queue()
 					.defer(d3.json, "https://d3js.org/us-10m.v2.json")
-					.defer(d3.csv, "static/data/data_source0.csv")
+					.defer(d3.csv, "data/data_source0.csv")
 					.await(ready);
 		}, 1000);
 	}
